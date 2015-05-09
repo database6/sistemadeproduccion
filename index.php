221a451b3ca5fbbc870aaf5188b6ec9b06e4cf47
Carlos A. Sánchez Correa <carliitozcorrea@gmail.com>
2015-05-09 00:08:14 -0500

    Nuevos Archivos
    Archivos Index y Conexion a la Base de Datos

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Sis. de Producción</title>
+<link href="resources/css/.css" media="screen" rel="stylesheet" type="text/css">
+</head>
+<body>
+<b>BIENVENIDO AL SISTEMA DE PRODUCCION</b>
+</body>
+</html>
---
+++ resources/javascript/habilita.js
@@ -0,0 +1,20 @@
+function habilita(){
+document.registro.lineadec.disabled=false;
+document.registro.lineadec.required=true;
+return S;
+
+}
+
+function deshabilita(){
+document.registro.lineadec.disabled=true;
+document.registro.lineadec.value="";
+document.registro.lineadec.required=false;
+return V;
+}
+
+
+
+
+
+
+
